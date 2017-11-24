009877023409f779776a08c6628ee2e1209de702
Ian J. Watson <ian.james.watson@cern.ch>
2017-11-24 12:54:22 +0900

    Add running on dilepton channel only

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+# Use singularity image to run make, i.e.
+# singularity ~/Images/Madgraph.img make run_tsW_dilep
+
 pythia_tsW : pythia_tsW.cc
 	g++ pythia_tsW.cc -o pythia_tsW `/code/MG5_aMC_v2_6_0/HEPTools/pythia8/bin/pythia8-config --cflags --libs` -I/code/MG5_aMC_v2_6_0/HEPTools/hepmc/include -L/code/MG5_aMC_v2_6_0/HEPTools/hepmc/lib -lHepMC -ldl
 
@@ -9,3 +12,17 @@ run_tsW : pythia_tsW
 
 run_tbW : pythia_tbW
 	LD_LIBRARY_PATH=/code/MG5_aMC_v2_6_0/HEPTools/hepmc/lib:$LD_LIBRARY_PATH ./pythia_tbW
+
+pythia_tsW_dilep : pythia_tsW_dilep.cc
+	g++ pythia_tsW_dilep.cc -o pythia_tsW_dilep `/code/MG5_aMC_v2_6_0/HEPTools/pythia8/bin/pythia8-config --cflags --libs` -I/code/MG5_aMC_v2_6_0/HEPTools/hepmc/include -L/code/MG5_aMC_v2_6_0/HEPTools/hepmc/lib -lHepMC -ldl
+
+pythia_tbW_dilep : pythia_tbW_dilep.cc
+	g++ pythia_tbW_dilep.cc -o pythia_tbW_dilep `/code/MG5_aMC_v2_6_0/HEPTools/pythia8/bin/pythia8-config --cflags --libs` -I/code/MG5_aMC_v2_6_0/HEPTools/hepmc/include -L/code/MG5_aMC_v2_6_0/HEPTools/hepmc/lib -lHepMC -ldl
+
+run_tsW_dilep : pythia_tsW_dilep
+	LD_LIBRARY_PATH=/code/MG5_aMC_v2_6_0/HEPTools/hepmc/lib:$LD_LIBRARY_PATH ./pythia_tsW_dilep
+	/code/MG5_aMC_v2_6_0/Delphes/DelphesHepMC delphes_card_CMS.tcl tsW_dilep.root tsW_dilep.hepmc
+
+run_tbW_dilep : pythia_tbW_dilep
+	LD_LIBRARY_PATH=/code/MG5_aMC_v2_6_0/HEPTools/hepmc/lib:$LD_LIBRARY_PATH ./pythia_tbW_dilep
+	/code/MG5_aMC_v2_6_0/Delphes/DelphesHepMC delphes_card_CMS.tcl tbW_dilep.root tbW_dilep.hepmc
---
+++ pythia_tbW_dilep.cc
@@ -0,0 +1,50 @@
+// singularity exec ~/Images/CCMadgraph.img make
+// singularity exec ~/Images/CCMadgraph.img ./pythia
+// cat tbW.hepmc | singularity exec ~/Images/CCMadgraph.img /code/MG5_aMC_v2_6_0/Delphes/DelphesHepMC ./Cards/delphes_card_CMS.dat tbW.root -
+
+#include "Pythia8/Pythia.h"
+#include "Pythia8Plugins/HepMC2.h"
+#include "HepMC/GenEvent.h"
+#include "HepMC/IO_GenEvent.h"
+
+using Pythia8::Pythia;
+
+int main()
+{
+  Pythia pythia("./pythia-xml");
+
+  pythia.readString("StandardModel:Vtb = 1.0");
+  pythia.readString("StandardModel:Vts = 0.0");
+  pythia.readString("StandardModel:Vtd = 0.0");
+
+  pythia.readString("Top:gg2ttbar = on");
+  pythia.readString("Top:qqbar2ttbar = on");
+
+  pythia.readString("Main:numberOfEvents = 10000");
+
+  pythia.readString("Beams:idA = 2212");
+  pythia.readString("Beams:idB = 2212");
+  pythia.readString("Beams:frameType = 1");
+  pythia.readString("Beams:eCM = 13000");
+
+  pythia.readString("24:onMode = 0"); // turn all W decays off
+  pythia.readString("24:onIfAny = 13"); // turn on mu decay mode
+  pythia.readString("24:onIfAny = 11"); // turn on electron decay mode
+
+  pythia.settings.listChanged();
+  HepMC::Pythia8ToHepMC ToHepMC;
+  HepMC::IO_GenEvent ascii_io("tbW_dilep.hepmc", std::ios::out);
+
+  pythia.init();
+  for (int iEvent = 0; iEvent < 10000; ++iEvent) {
+    pythia.next();
+    HepMC::GenEvent* hepmcevt = new HepMC::GenEvent();
+    ToHepMC.fill_next_event(pythia, hepmcevt);
+    ascii_io << hepmcevt;
+    delete hepmcevt;
+  }
+
+  pythia.stat();
+  
+  return 0;
+}
