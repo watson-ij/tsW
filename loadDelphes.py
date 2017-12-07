c8a58ce8078bc9cbdb27550651540c8b00a7df0d
Ian J. Watson <ian.james.watson@cern.ch>
2017-12-07 13:50:07 +0900

    Fix loadDelphes.py for singularity container

--- loadDelphes.py
+++ loadDelphes.py
@@ -1,4 +1,8 @@
 import ROOT
 
-ROOT.gSystem.Load("/home/iwatson/install/delphes/libDelphes.so")
+ROOT.gInterpreter.AddIncludePath('/code/MG5_aMC_v2_6_0/Delphes/external/')
+ROOT.gInterpreter.AddIncludePath('/code/MG5_aMC_v2_6_0/Delphes/external/ExRootAnalysis')
+ROOT.gInterpreter.AddIncludePath('/code/MG5_aMC_v2_6_0/Delphes/')
+ROOT.gInterpreter.AddIncludePath('/code/MG5_aMC_v2_6_0/Delphes/classes/')
 ROOT.gInterpreter.Declare('#include "classes/DelphesClasses.h"')
+ROOT.gSystem.Load("/code/MG5_aMC_v2_6_0/Delphes/libDelphes.so")
